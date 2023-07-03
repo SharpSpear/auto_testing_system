b70a544af876907bafba5bf438ca3e9d89bdfff3
sharpspear <spearbig1021@gmail.com>
2023-07-04 01:16:43 +0900

    update

--- runserver.py
+++ runserver.py
@@ -11,6 +11,7 @@
 import os
 from src import app
 
+
 if __name__ == "__main__":
     HOST = os.environ.get("SERVER_HOST", "0.0.0.0")
     ssl_context=('cert.pem', 'key.pem')
@@ -18,5 +19,7 @@ if __name__ == "__main__":
         PORT = int(os.environ.get("SERVER_PORT", "5555"))
     except ValueError:
         PORT = 1234
-    # app.secret_key = "1cd6f35db029d4b8fc98fc05c9efd06a2e2cd1ffc3774d3f035ebd8d"
-    app.run(HOST, PORT, debug=True)
+    app.secret_key = "1cd6f35db029d4b8fc98fc05c9efd06a2e2cd1ffc3774d3f035ebd8d"
+    app.run(HOST, PORT, debug=True, ssl_context=ssl_context)
+
+
